0a25d5be71e62b050cc98f87deffbd6bd0d9f4fa
OthmanAlrasheedi <othman.mousa.rae.js@tuwaiq.edu.sa>
2022-02-21 14:24:01 +0300

    Lab set

---
+++ Lab_Set.py
@@ -0,0 +1,14 @@
+visit_Ahmed={"Rafha","jeedh","dubai","paris"}
+visit_Faris={"Rafha","jeedh","New York","London"}
+
+
+
+print("set uniom")
+print(visit_Ahmed.union(visit_Faris))
+
+print("set intersection")
+print(visit_Ahmed.intersection(visit_Faris))
+
+
+print("difference")
+print(visit_Faris.difference(visit_Ahmed))
