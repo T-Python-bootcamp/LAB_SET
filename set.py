4c2dc26f73a327fbbf85d20cb616abbabb9263e5
jamelhfarhan <jamelh.farhan.rae.js@tuwaiq.edu.sa>
2022-02-21 14:22:55 +0300

    new solve

---
+++ set.py
@@ -0,0 +1,7 @@
+citie_ahmad= {"Hail", "Riyadh", "Dubai"}
+citie_Faris={"Riyadh", "Jizan", "Abu Dhabi", "Hail"}
+print(citie_ahmad |(citie_Faris))
+
+print(citie_ahmad&(citie_Faris))
+
+print(citie_ahmad.difference(citie_Faris))
